288904ad28d0f48c6dfe0ce78d25f8e551b60678
prajwalcool <prajwal331@gmail.com>
2020-05-16 10:50:34 +0530

    created backend

---
+++ backend/backend/routers.py
@@ -0,0 +1,4 @@
+from rest_framework import routers
+from products.viewsets import ProductViewSet
+router = routers.DefaultRouter()
+router.register(r'product', ProductViewSet)
---
+++ backend/products/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+    """[Category Model]
+"""
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+    is_indian = models.BooleanField(default=False)
+
+
+class Product(models.Model):
+    """[Product Model]
+"""
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
