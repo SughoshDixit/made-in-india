4204f2fe7696689c5370c1e96368ecd22b19d032
prajwalcool <prajwal331@gmail.com>
2020-06-19 21:47:06 +0530

    Ok

---
+++ frontend/components/Products.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="product">
+    {{ product.id }}
+    {{ product.name }}
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { PropOptions } from 'vue/types/umd'
+import { ProductType } from '~/graphql/types'
+
+export default Vue.extend({
+  name: 'Products',
+  props: {
+    product: { type: Object, default: () => {} } as PropOptions<ProductType>
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.product {
+  background: white;
+  padding: 1rem;
+  margin: 1rem 0;
+  border-radius: 15px;
+}
+</style>
---
+++ frontend/pages/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="container">
+    <div v-if="products">
+      <products
+        v-for="product in products"
+        :key="product.id"
+        :product="product"
+      ></products>
+    </div>
+    hi
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Products from '~/components/Products.vue'
+import ProductsGQL from '~/graphql/products.gql'
+import { ProductType } from '~/graphql/types'
+
+interface Data {
+  products?: ProductType[]
+}
+
+export default Vue.extend({
+  components: { Products },
+  data(): Data {
+    return {
+      // products: [
+      //   {
+      //     id: 'AD 156',
+      //     name: 'Dabur Red',
+      //     logo: 'www.daburred.com',
+      //     description: 'Ayurvedic Toothpaste',
+      //     isIndian: true,
+      //     urls: ['www.daburred.com', 'www.daburred.com'],
+      //     keywords: ['Tooth Paste', 'Tooth Brush']
+      //   },
+      //   {
+      //     id: 'AD 156',
+      //     name: 'Dabur Red',
+      //     logo: 'www.daburred.com',
+      //     description: 'Ayurvedic Toothpaste',
+      //     isIndian: true,
+      //     urls: ['www.daburred.com', 'www.daburred.com'],
+      //     keywords: ['Tooth Paste', 'Tooth Brush']
+      //   },
+      //   {
+      //     id: 'AD 156',
+      //     name: 'Dabur Red',
+      //     logo: 'www.daburred.com',
+      //     description: 'Ayurvedic Toothpaste',
+      //     isIndian: true,
+      //     urls: ['www.daburred.com', 'www.daburred.com'],
+      //     keywords: ['Tooth Paste', 'Tooth Brush']
+      //   }
+      // ],
+      products: undefined
+    }
+  },
+  apollo: {
+    products: ProductsGQL
+  }
+})
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+</style>
